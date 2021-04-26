fdd51bd1f6d8ed5c07b17ecf1d2ab76aa1410777
Jake Hansen <jakedhansen@outlook.com>
2021-04-26 14:26:10 -0500

    Add login servlet and status

--- src/main/java/com/jakedhansen/procuratio/services/UserService.java
+++ src/main/java/com/jakedhansen/procuratio/services/UserService.java
@@ -1,11 +1,16 @@
 package com.jakedhansen.procuratio.services;
 
 import com.jakedhansen.procuratio.domain.User;
+import com.jakedhansen.procuratio.dto.Credentials;
 import com.jakedhansen.procuratio.dto.NewUser;
 
 public class UserService {
 
     public static User Register(NewUser user) throws Exception {
+        throw new RuntimeException();
+    }
+
+    public static User Login(Credentials credentials) throws Exception {
         throw new RuntimeException();
     }
 }
---
+++ src/main/java/com/jakedhansen/procuratio/servlets/Login.java
@@ -0,0 +1,46 @@
+package com.jakedhansen.procuratio.servlets;
+
+import com.jakedhansen.procuratio.domain.User;
+import com.jakedhansen.procuratio.dto.Credentials;
+import com.jakedhansen.procuratio.services.UserService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Login", value = "/login")
+public class Login extends HttpServlet {
+    public enum Status {
+        SUCCESSFUL,
+        BAD_PASSWORD,
+        FAILURE
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        Credentials credentials = new Credentials(username, password);
+
+        HttpSession session = request.getSession();
+
+        try {
+            User registeredUser = UserService.Login(credentials);
+            session.setAttribute("login_status", Status.SUCCESSFUL);
+
+            response.sendRedirect("index.jsp");
+        } catch (Exception e) {
+            session.setAttribute("login_status", Status.FAILURE);
+            session.setAttribute("login_username", credentials.getUsername());
+
+            response.sendRedirect("login.jsp");
+        }
+    }
+}
